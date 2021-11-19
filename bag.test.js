9d5fa67ebd7cb95d346879a4b95704747748819f
MartinRenteria <renteriamp96@gmail.com>
2021-11-19 09:07:30 -0800

    Adding additional test for bag class for bag weight input

--- bag.test.js
+++ bag.test.js
@@ -7,4 +7,7 @@ describe('Bag objects', () => {
 		expect(testBag.weight).toBe(50)
 	})
 
+    test("This will throw an error if bag doesn't have weight", () => {
+		expect(() => new Bag()).toThrowError('bag must have a weight')
+	})
 })
