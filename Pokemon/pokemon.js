81faa7a754002408a4ba0d080ee4e129d00dbc11
MartinRenteria <renteriamp96@gmail.com>
2021-11-22 09:55:17 -0800

    Adding finished methods for pokemon class

--- Pokemon/pokemon.js
+++ Pokemon/pokemon.js
@@ -1,31 +1,38 @@
 class Pokemon {
-    static pokemons = []
-  
-    constructor(name, number, type, hp){
-        this.name = name
-        this.number = number
-        this.type = type
-        this.hp = hp
-    }
-  
-    static nameIsString(pokemon) {
-        
-    }
-  
-    static numberIsTypeNumber(pokemonNumber) {
-    }
-  
-    static isValidType(type) {
-      const elements = []
-    }
-  
-    static isValidHp(hp) {
-    }
-  
-    static isValidPokemon(name,number,type,hp) {
-    }
-    assignType() {
-  
-    }
-  
+  static pokemons = [];
+
+  constructor(name, number, type, hp) {
+    this.name = name;
+    this.number = number;
+    this.type = type;
+    this.hp = hp;
   }
+
+  static nameIsString(name) {
+    return typeof name === 'string'
+  }
+
+  static numberIsTypeNumber(pokemonNumber) {
+    return typeof pokemonNumber.number === 'number'
+
+  }
+
+  static isValidType(type) {
+    const elements = ['fire','water','electric', 'ice'];
+    return elements.includes(type)
+  }
+
+  static isValidHp(hp) {
+    return number > 0 && number <= 999
+  }
+
+  static isValidPokemon(pokemon) {
+    return Pokemon.isValidPokemon(pokemon.name) && Pokemon.isValidPokemon(pokemon.pokemonNumber) && Pokemon.isValidPokemon(pokemon.isValidType) && Pokemon.isValidPokemon(pokemon.isValidHp)
+  }
+  assignType() {}
+}
+
+let pikachu = new Pokemon('pika', 3, 'Animal', 25)
+console.log(pikachu)
+console.log(Pokemon.nameIsString('pokemon'))
+console.log(Pokemon.isValidType('water'))
--- static-fuctions/static-functions.js
+++ static-fuctions/static-functions.js
@@ -1,28 +1,27 @@
-
 class Square {
-    constructor(length) {
-      this.width = length;
-      this.height = length;
-    }
-  
-    static checkIsValidSquare(width, height) {
-      return width === height;
-    }
-  
-    static isAreaEqual(squareA, squareB) {
-      const areaA = squareA.width * squareA.height;
-      const areaB = squareB.width * squareB.height;
-  
-      return areaA === areaB;
-    }
-  
+  constructor(length) {
+    this.width = length;
+    this.height = length;
   }
 
-  let s1 = new Square(4)
-  let s2 = new Square(4)
+  static checkIsValidSquare(width, height) {
+    return width === height;
+  }
 
-  console.log(s1)
-  console.log(s2)
-  console.log(Square.isAreaEqual(1, 1))
-  console.log(Square.isAreaEqual(10, 10))
-  console.log(Square.isAreaEqual(s1, s2))
+  static isAreaEqual(squareA, squareB) {
+    const areaA = squareA.width * squareA.height;
+    const areaB = squareB.width * squareB.height;
+
+    return areaA === areaB;
+  }
+}
+
+let s1 = new Square(4);
+let s2 = new Square(4);
+
+console.log(s1);
+console.log(s2);
+console.log(Square.checkIsValidSquare(5))
+console.log(Square.isAreaEqual(1, 1));
+console.log(Square.isAreaEqual(10, 10));
+console.log(Square.isAreaEqual(s1, s2));
