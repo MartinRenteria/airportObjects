a02c8fc0c0c0d740136cba85da6d36a30fa90a24
MartinRenteria <renteriamp96@gmail.com>
2021-11-22 08:34:43 -0800

    Created folders for files and added pokemon class

---
+++ Pokemon/pokemon.js
@@ -0,0 +1,31 @@
+class Pokemon {
+    static pokemons = []
+  
+    constructor(name, number, type, hp){
+        this.name = name
+        this.number = number
+        this.type = type
+        this.hp = hp
+    }
+  
+    static nameIsString(pokemon) {
+        
+    }
+  
+    static numberIsTypeNumber(pokemonNumber) {
+    }
+  
+    static isValidType(type) {
+      const elements = []
+    }
+  
+    static isValidHp(hp) {
+    }
+  
+    static isValidPokemon(name,number,type,hp) {
+    }
+    assignType() {
+  
+    }
+  
+  }
